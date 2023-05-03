11c79d746b26aaf2b1dd7650d19ee54b70de9952
CarolineCRodrigues <caroline.rodrigues@ticapp.gov.pt>
2023-05-03 22:26:48 +0100

    Implicit animation Meals

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -35,8 +35,21 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(
-              isFavorite ? Icons.star : Icons.star_border,
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(
+                    begin: 0.8,
+                    end: 1.0,
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           ),
         ],
